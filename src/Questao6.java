d45744ffc3341f64a7e6343614e216adcfea706e
Pedro Rickson <pedrorickson95.fa@gmail.com>
2023-03-24 17:17:20 -0300

    6) Verifique se A é maior que 10, se verdade imprima: "A > 10" caso não seja, imprima: "A <= 10". Verifique se A mais B é igual a 20, se verdadeiro, imprima: "A + B == 20”, caso não seja imprima: “A + B != 20”.

---
+++ src/Questao6.java
@@ -0,0 +1,22 @@
+
+public class Questao6 {
+
+	public static void main(String[] args) {
+	
+		int a = 5;
+		int b = 12;
+		
+		if(a > 10 ) {
+			System.out.println("A > 10.");
+		}else {
+			System.out.println("A <= 10.");
+		}
+		
+		if(a + b == 20) {
+			System.out.println("A + B == 20");
+		}else {
+			System.out.println("A + B != 20");
+		}
+	}
+
+}
