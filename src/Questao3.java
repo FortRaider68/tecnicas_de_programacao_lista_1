a2d275a5c7c9e6cf12098c2f70c3ba2b7cf47c90
Pedro Rickson <pedrorickson95.fa@gmail.com>
2023-03-24 16:29:44 -0300

    3) Verifique se A é igual a 10, se verdade imprima: "A == 10", e verifique se o A mais B é igual a 20, se verdade imprima: "A + B == 20". verifique se o B é igual a 10, se verdade imprima: "B == 10", (obs: todas as três impressões são permitidas na saída do programa).

---
+++ src/Questao3.java
@@ -0,0 +1,19 @@
+
+public class Questao3 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 10;
+		
+		if(a == 10)
+			System.out.println("A == 10");
+		
+		if(a + b == 20)
+			System.out.println("A + B == 20");
+		
+		if(b == 10)
+			System.out.println("B == 10");
+		
+	}
+
+}
