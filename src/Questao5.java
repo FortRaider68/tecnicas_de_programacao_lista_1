c896628dbe03c59be3d1f442dbc972ec7a46c93b
Pedro Rickson <pedrorickson95.fa@gmail.com>
2023-03-24 17:16:30 -0300

    5) Verifique se A é maior que 10 ou se A mais B é igual a 20, se verdade imprima: "número válido". Caso as afirmações não sejam verdadeiras, imprima: "número não válido".

---
+++ src/Questao5.java
@@ -0,0 +1,17 @@
+
+public class Questao5 {
+
+	public static void main(String[] args) {
+
+		int a = 9;
+		int b = 12;
+		
+		if(a > 10 || a + b == 20) {
+			System.out.println("número válido.");
+			return;
+		}
+		
+		System.out.println("número não válido.");
+	}
+
+}
