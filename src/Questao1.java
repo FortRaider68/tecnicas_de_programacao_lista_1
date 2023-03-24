f3f130f33cf5e9c3c413392b9af26bd0af90e0ac
Pedro Rickson <pedrorickson95.fa@gmail.com>
2023-03-24 15:02:06 -0300

    Verifique se A é maior que 10, se verdade imprima: "A > 10" e verifique se o A mais B é igual a 20, se verdade imprima: "A + B == 20".Caso a segunda afirmação não seja verdadeira, imprima: "número não válido".

---
+++ src/Questao1.java
@@ -0,0 +1,19 @@
+
+public class Questao1 {
+	public static void main(final String[] args)
+	{
+		int a = 12;
+		int b = 4;
+		
+		if (a > 10)
+			System.out.println("A > 10");	
+		
+		if(a + b == 20) {
+			System.out.println("A + B == 20");
+		}else {
+			System.out.println("número não válido");
+		}
+			
+		
+	}
+}
