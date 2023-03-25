339f958e9cf6197b239ef22d5617b92904bad864
Pedro Rickson <pedrorickson95.fa@gmail.com>
2023-03-25 12:15:57 -0300

    11) Verifique se A é maior que 10 se não for verifique se A mais B é igual a 20, imprima: "A + B == 20''. Caso A não seja maior que 10 e A mais B for diferente de 20, imprima: "número não válido".

---
+++ Lista1/src/Questao11.java
@@ -0,0 +1,19 @@
+
+public class Questao11 {
+
+	public static void main(String[] args) {
+		
+		int a = 2;
+		int b = 34;
+		
+		if(a <= 10) {
+			if(a + b == 20) {
+				System.out.println("A + B == 20");
+			}
+		}else {
+			System.out.println("número não válido");
+		}
+
+	}
+
+}
