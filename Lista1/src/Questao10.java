3b568ba65ba24e590af467596586724a437f47a9
Pedro Rickson <pedrorickson95.fa@gmail.com>
2023-03-25 12:14:50 -0300

    10)Verifique se A é maior que 10 se não for imprima: "número menor que 10". Verifique se A mais B é igual a 20 se não for verdade imprima "número diferente de 20".

---
+++ Lista1/src/Questao10.java
@@ -0,0 +1,16 @@
+
+public class Questao10 {
+
+	public static void main(String[] args) {
+		
+		int a = 12;
+		int b = 34;
+		
+		if(a < 10)
+			System.out.println("número menor que 10");
+		
+		if(a + b != 20)
+			System.out.println("número diferente de 20");
+	}
+
+}
