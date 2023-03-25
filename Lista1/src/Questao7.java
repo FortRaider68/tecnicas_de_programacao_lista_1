69e471a5da249521fb38f84b1730506e11d39d5e
Pedro Rickson <pedrorickson95.fa@gmail.com>
2023-03-24 23:03:47 -0300

    7) Verifique se A é maior que 10 ou se o A mais B é igual a 20, caso sejam verdadeiras as afirmações imprima: "números válidos”. Caso a primeira afirmação não seja verdadeira, imprima: "número não válido".

---
+++ Lista1/src/Questao7.java
@@ -0,0 +1,19 @@
+
+public class Questao7 {
+
+	public static void main(String[] args) {
+		
+		int a = 32;
+		int b = -12;
+		
+		if(a > 10) {
+			if(a + b == 20) {
+				System.out.println("números válidos.");
+			}
+		}else {
+			System.out.println("número não válido.");
+		}
+
+	}
+
+}
