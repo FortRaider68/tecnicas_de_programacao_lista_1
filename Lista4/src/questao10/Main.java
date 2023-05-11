b8c882c9b3f1b0af4c16e5aadb518c686a991169
Pedro Rickson <pedrorickson95.fa@gmail.com>
2023-05-11 11:13:34 -0300

    Questão 10

---
+++ Lista4/src/questao10/Main.java
@@ -0,0 +1,11 @@
+package questao10;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Empregado emp = new Empregado("João", 24, 56, 2300.0, 120.0);
+		System.out.println("Salario:"+emp.calcularSalario());
+
+	}
+
+}
---
+++ Lista4/src/questao10/Pessoa.java
@@ -0,0 +1,14 @@
+package questao10;
+
+public class Pessoa {
+	public String nome;
+	public int idade;
+
+	public Pessoa(String nome, int idade) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+	}
+	
+	
+}
