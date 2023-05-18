f61dd666558fd8340cea8a5d4114fb7771ff9fc1
Pedro Rickson <pedrorickson95.fa@gmail.com>
2023-05-18 19:19:57 -0300

    Jogo da Advinhação

---
+++ JogoDeAdivinhacao/src/negocio/Jogo.java
@@ -0,0 +1,93 @@
+package negocio;
+
+import apresentacao.*;
+
+public class Jogo {
+
+	private Jogador jogador;
+	private Tela tela;
+	private Terminal terminal;
+	private int numeroEscolhido;
+	private boolean jogando;
+
+	public Jogo() {
+		tela = new Tela();
+		terminal = new Terminal();
+		jogador = new Jogador();
+		numeroEscolhido = 50;
+		jogando = true;
+	}
+
+	public void inciarJogoTerminal() {
+		jogador.setNome(terminal.entradaNome());
+		terminal.mesagem("Seja bem vindo: " + jogador.getNome());
+	}
+
+	public void inciarJogoGUI() {
+		jogador.setNome(tela.entradaDados("Qual é o seu nome?"));
+		tela.mesagem("Seja bem vindo: " + jogador.getNome());
+		jogadas();
+	}
+
+	public int solicitarNumero() {
+		String numero = tela.entradaDados("Informe um numero:");
+		return Integer.parseInt(numero);
+	}
+
+	public void jogadas() {
+		do {
+			verificarAcerto();
+		} while (jogando);
+
+		fimDoJogo();
+	}
+
+	private void fimDoJogo() {
+
+		String numeros = "";
+		for (Integer numero : jogador.getListaNumeros()) {
+			numeros += " - " + numero;
+		}
+		tela.mesagem("Numeros apostados: " + numeros);
+
+	}
+
+	public boolean verificarMenor(int numero) {
+		if (numero < numeroEscolhido)
+			return true;
+
+		return false;
+	}
+
+	public void verificarAcerto() {
+		int numero = solicitarNumero();
+		jogador.addNumero(numero);
+		if (numero == numeroEscolhido) {
+			tela.mesagem("Parabens voce acertou! numero de tentativas: " + jogador.getNumeroTentativa());
+			jogando = false;
+		} else {
+			tela.mesagem("Deu ruim,  voce errou");
+			jogador.setNumeroTentativa();
+
+			if (verificarMenor(numero)) {
+				tela.mesagem("tente um numero maior");
+			} else {
+				tela.mesagem("Tente numero menor");
+			}
+		}
+	}
+
+	// public void verificarAcerto() {
+	// do {
+	// if (solicitarNumero() == numeroEscolhido) {
+	// jogador.setNumeroTentativa();
+	// tela.mesagem("Parabens voce acertou! numero de tentativas: " +
+	// jogador.getNumeroTentativa());
+	// return;
+	// } else {
+	// tela.mesagem("Deu ruim, voce errou");
+	// jogador.setNumeroTentativa();
+	// }
+	// } while (true);
+	// }
+}
